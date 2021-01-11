4709e7a01c5be3acbec33e5a934b52ad29a163b0
AnshulFortnitePro <76525361+AnshulFortnitePro@users.noreply.github.com>
2021-01-11 18:56:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+
+var wall, thickness;
+var bullet, speed, weight;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+ 
+  thickness = random(22,83);
+  speed = random(223, 321);
+  weight = random(30, 52);
+
+  wall = createSprite(1400, 200, thickness, height/2);
+
+
+  bullet = createSprite(200, 200, 30, 10);
+  bullet.shapeColor = "black";
+  bullet.velocityX = speed;
+
+}
+
+function draw() {
+  
+  background("yellow");  
+
+
+  if(hasCollided(bullet, wall)){
+   
+    bullet.velocityX = 0;
+   
+    var damage = 0.5 * weight * speed * speed/(thickness* thickness* thickness);
+
+    
+    if(damage>10){
+      wall.shapeColor = color(255,0,0);
+    }
+  }
+ 
+   if(damage<10){
+     wall.shapeColor = color(0, 255, 0);
+   }
+
+  drawSprites();
+}
+
+function hasCollided(bullet, wall){
+  bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+    return false;
+  
+  }
